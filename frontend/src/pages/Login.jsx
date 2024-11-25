0d1f4e7fde4dfb4b296d61897db5f176b97d5e3e
nivin <bnivin71@gmail.com>
2024-11-25 10:56:35 +0530

    redux crud

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Form from '../components/Form'
+
+function Login() {
+  return (
+    <Form route="/users/token/" method="login"/>
+  )
+}
+
+export default Login
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Form from '../components/Form'
+
+function Register() {
+  return (
+    <Form route="/users/user/register/" method="register"/>
+  )
+}
+
+export default Register
